7f95bbde0a01b4b7c7a98e33972fd8f4de86a47d
ecotner-desktop <2.71828cotner@gmail.com>
2017-12-19 16:48:33 -0800

    updated car rental script to include altered reward structure, generated value/policy graphs

--- ReinforcementLearning/RLExercises/CarRental/CarRental.py
+++ ReinforcementLearning/RLExercises/CarRental/CarRental.py
@@ -5,6 +5,8 @@ Created on Mon Dec 18 13:29:52 2017
 
 Problem: Jack runs two car rental locations, and must determine how to distribute his rental cars between the two locations. He earns $10 for each car rental, and they become available for rental the day after they are returned. It costs him $2 to move cars between each location. The number of cars requested at each location is a Poisson random variable n. To simplify the problem, we assume that each location can hold a maximum number of cars, and similarly, only a maximum number can be transferred between each location on a given day.
 
+We can then add in some additional reward structure. One of Jack’s employees at the first location rides a bus home each night and lives near the second location. She is happy to shuttle one car to the second location for free. Each additional car still costs $2, as do all cars moved in the other direction. In addition, Jack has limited parking space at each location. If more than 5 cars are kept overnight at a location (after any moving of cars), then an additional cost of $4 must be incurred to use a second parking lot (independent of how many cars are kept there).
+
 @author: ecotner
 """
 
@@ -33,6 +35,7 @@ class Environment(object):
     Class representing the environment; in this case, the car rental business. The states in which the environment can exist consist of an array numbers representing the number of cars at each location. The transistions between states are the result of 1) the number of cars the agent decides to transfer between locations 2) the number of cars rented/returned that day.
     '''
     def __init__(self):
+        self.is_original = False
         self.n_locations = 2
         self.max_cars = 10
         self.max_transfer = 5
@@ -40,6 +43,7 @@ class Environment(object):
         self.mean_returned = [3,2]
         self.reward_rented = 10
         self.reward_trans = -2
+        self.reward_overnight_parking = -4
         assert self.n_locations == len(self.mean_rented) == len(self.mean_returned), "Number of locations inconsistent"
         self.states = [(i,j) for i in range(self.max_cars) for j in range(self.max_cars)]
         self.value = np.zeros([self.max_cars for i in range(self.n_locations)])
@@ -62,7 +66,17 @@ class Environment(object):
                                     # Calculate number of cars in each lot after state transition, and reward from new state
                                     n1_ = min(self.max_cars, max(0, n1+a-n1_rented)+n1_returned)
                                     n2_ = min(self.max_cars, max(0, n2-a-n2_rented)+n2_returned)
-                                    r = self.reward_rented*(min(n1+a,n1_rented) + min(n2-a,n2_rented)) + self.reward_trans*abs(a)
+                                    if self.is_original:
+                                        r = self.reward_rented*(min(n1+a,n1_rented) + min(n2-a,n2_rented)) + self.reward_trans*abs(a)
+                                    else:
+                                        if a > 0:
+                                            r = self.reward_rented*(min(n1+a,n1_rented) + min(n2-a,n2_rented)) + self.reward_trans*a
+                                        else:
+                                            r = self.reward_rented*(min(n1+a,n1_rented) + min(n2-a,n2_rented)) + self.reward_trans*(a+1)
+                                        if n1_ > self.max_cars//2:
+                                            r += self.reward_overnight_parking
+                                        if n2_ > self.max_cars//2:
+                                            r += self.reward_overnight_parking
                                     # Calculate probability of transition occuring this way
                                     prob_ = clipped_poisson(n1_rented, self.mean_rented[0],min(self.max_cars,n1+a))*clipped_poisson(n2_rented, self.mean_rented[1],min(self.max_cars,n2-a))*clipped_poisson(n1_returned, self.mean_returned[0],self.max_cars-(n1+a-n1_rented))*clipped_poisson(n2_returned, self.mean_returned[1],self.max_cars-(n2-a-n2_rented))
                                     # Since new states are combinations of different ways the transition can happen, we need to sum the probabilities of all the different ways a given initial state can end up in the same final state
@@ -79,7 +93,7 @@ def check_normalization(n1, n2, a):
     p = 0
     for n1_ in range(env.max_cars+1):
         for n2_ in range(env.max_cars+1):
-            for r in range(env.reward_trans*env.n_locations*env.max_transfer, env.reward_rented*env.n_locations*env.max_cars+1):
+            for r in range(2*env.reward_overnight_parking + env.reward_trans * env.n_locations * env.max_transfer, env.reward_rented * env.n_locations * env.max_cars+1):
                 p += prob.get((n1_,n2_,r,n1,n2,a), 0)
     return p
 
@@ -105,7 +119,7 @@ def policy_evaluation(v1, policy):
         v0 = v1
         for n1 in range(env.max_cars+1):
             for n2 in range(env.max_cars+1):
-                for r in range(env.reward_trans*env.n_locations*env.max_transfer, env.reward_rented*env.n_locations*env.max_cars+1):
+                for r in range(env.n_locations * env.reward_overnight_parking + env.reward_trans * env.n_locations * env.max_transfer, env.reward_rented * env.n_locations * env.max_cars+1):
                     for n1_ in range(env.max_cars+1):
                         for n2_ in range(env.max_cars+1):
                             v2[(n1,n2)] += prob.get((n1_,n2_,r,n1,n2,policy[(n1,n2)]), 0) * (r + gamma*v1[(n1_,n2_)])
@@ -123,7 +137,7 @@ def policy_improvement(v, policy):
                 q = 0
                 for n1_ in range(env.max_cars+1):
                     for n2_ in range(env.max_cars+1):
-                        for r in range(env.reward_trans*env.n_locations*env.max_transfer, env.reward_rented*env.n_locations*env.max_cars+1):
+                        for r in range(env.n_locations * env.reward_overnight_parking + env.reward_trans * env.n_locations * env.max_transfer, env.reward_rented * env.n_locations * env.max_cars+1):
                             q += prob.get((n1,n2,r,n1_,n2_,a),0) * (r + gamma*v[(n1_,n2_)])
                 Q.append(q)
             policy[(n1,n2)] = np.argmax(Q)-5 # Offset since index 0 of Q corresponds to action of -5
@@ -132,7 +146,7 @@ def policy_improvement(v, policy):
     return policy, policy_stable
 
 def policy_iteration():
-    # Initialize blank policy and value function
+    # Initialize "blank" policy and value function
     pi0 = {(n1,n2):0 for n1 in range(env.max_cars+1) for n2 in range(env.max_cars+1)}
     v0 = {(n1,n2):10 for n1 in range(env.max_cars+1) for n2 in range(env.max_cars+1)}
     policy_stable = False
--- ReinforcementLearning/RLExercises/CarRental/RADME.md
+++
@@ -1,3 +0,0 @@
-Solution to exercise 4.5.
-
-Problem: Jack runs two car rental locations, and must determine how to distribute his rental cars between the two locations. He earns $10 for each car rental, and they become available for rental the day after they are returned. It costs him $2 to move cars between each location. The number of cars requested at each location is a Poisson random variable n. To simplify the problem, we assume that each location can hold a maximum number of cars, and similarly, only a maximum number can be transferred between each location on a given day.
