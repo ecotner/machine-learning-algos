f260e738d438b980d360405ce156e66d3ce9b5d9
ecotner <2.71828cotner@gmail.com>
2018-07-05 22:52:38 -0700

    added NaiveBayesLinear.py file, outlined skeleton code for later

---
+++ Supervised/Classification/SpamClassifier/NaiveBayesLinear.py
@@ -0,0 +1,37 @@
+"""
+Date: July 5, 2018
+Author: Eric Cotner
+
+Script for building, training, and running inference on a Naive Bayes classifier for classifying spam.
+"""
+
+# Import necessary modules
+from utils import importData, formatAllMessages, makeWordMapping
+
+# Define function for tokenizing message strings
+
+# Define the Model class
+    # Initialize empty untrained model
+
+    # Define Monitor class for monitoring progress during training
+        # Initialize class
+        # Define method for updating state of monitor
+        # Define method for outputting data to file
+
+    # Define training routine
+
+    # Define inference/prediction routine
+
+# Run the script
+if __name__ == "__main__":
+    pass
+    # Import raw data
+    # Convert data into usable format by tokenizing
+    # Split into train, validation, and test sets
+    # Add multiples of minority class (spam) to training set to balance classes
+
+    # Instantiate model
+    # Call training method
+    # Save model to file
+
+    # Run inference (do this later, after successful training)
